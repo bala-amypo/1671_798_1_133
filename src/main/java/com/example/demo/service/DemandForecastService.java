497bee4c6eb14b97eae6be6d25461fd4ee6f275d
bala-amypo <bala@amypo.in>
2025-12-22 02:35:41 +0000

    User commit

--- src/main/java/com/example/demo/service/DemandForecastService.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.service;
-
-import com.example.demo.entity.DemandForecast;
-
-public interface DemandForecastService {
-    DemandForecast createForecast(DemandForecast forecast);
-    DemandForecast getForecast(Long storeId, Long productId);
-}
