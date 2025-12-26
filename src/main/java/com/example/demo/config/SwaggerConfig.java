5fa5a45ae94bc3259fdaf416cba905fa3e50045f
bala-amypo <bala@amypo.in>
2025-12-26 06:00:48 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,28 +1,20 @@
 package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
-
-        SecurityScheme bearerScheme = new SecurityScheme()
-                .type(SecurityScheme.Type.HTTP)
-                .scheme("bearer")
-                .bearerFormat("JWT");
-
         return new OpenAPI()
-                .info(new Info()
-                        .title("Multi-Location Inventory Balancer API")
-                        .version("1.0"))
-                .addSecurityItem(new SecurityRequirement().addList("BearerAuth"))
-                .schemaRequirement("BearerAuth", bearerScheme);
-    }
+                // You need to change the port as per your server
+                .servers(List.of(
+                        new Server().url("https://9196.pro604cr.amypo.ai/")
+                ));
+        }
 }
