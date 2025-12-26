0015fe51f540dca6da0673f035ee77800ad6f377
bala-amypo <bala@amypo.in>
2025-12-26 10:34:44 +0000

    User commit

--- src/main/java/com/example/demo/controller/InventoryController.java
+++ src/main/java/com/example/demo/controller/InventoryController.java
@@ -1,21 +1,31 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.InventoryRequest;
 import com.example.demo.entity.InventoryLevel;
-import com.example.demo.service.InventoryLevelService;
+import com.example.demo.service.InventoryService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/inventory")
 public class InventoryController {
 
-    private final InventoryLevelService inventoryService;
+    private final InventoryService inventoryService;
 
-    public InventoryController(InventoryLevelService inventoryService) {
+    public InventoryController(InventoryService inventoryService) {
         this.inventoryService = inventoryService;
     }
 
     @PostMapping
-    public InventoryLevel createInventory(@RequestBody InventoryLevel inventory) {
-        return inventoryService.createOrUpdateInventory(inventory);
+    public ResponseEntity<InventoryLevel> createInventory(
+            @RequestBody InventoryRequest req) {
+
+        InventoryLevel saved = inventoryService.createOrUpdateInventory(
+                req.getStoreId(),
+                req.getProductId(),
+                req.getQuantity()
+        );
+
+        return ResponseEntity.ok(saved);
     }
 }
